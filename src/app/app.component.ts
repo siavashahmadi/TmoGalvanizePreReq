5eaf178624293dca8bf08eef91431ee9a369cfc2
Siavash Ahmadi <siavash.ahmadi2@t-mobile.com>
2022-04-06 20:40:32 -0400

    Siavash Ahmadi - Pre Assessment 1st Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pa-starter-angular';
+  recipeName = '';
+  recipeInstructions = '';
+  addParameter:boolean=false;
+  formData!: FormGroup;
+
+  ngOnInit() {
+    this.formData = new FormGroup({
+      Name: new FormControl('Al Pastor Tacos'),
+      Instructions: new FormControl('1. Cook Pork, 2. Heat Tortillas')
+    })
+  }
+
+  toggleForm() {
+    // hides form, initially, for inputting new recipes
+    // also used for hiding 'Add Recipe' button after click
+    this.addParameter = !this.addParameter
+  }
+
+  onSubmit() {
+    console.log("recipe form submitted!!!")
+    
+    console.log(this.formData.value)
+    
+    this.formData.setValue
+
+    console.log(this.formData.value)
+  }
+
 }
